d369fe52311c4092aa49f0b731da03db24b6c2bc
PupCat <105567668+PupCat@users.noreply.github.com>
2023-11-09 13:32:14 +0900

    Title: 문자열의 특정 위치 찾기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231109/문자열의 특정 위치 찾기 2/find-specific-location-in-spring-2.c
+++ 231109/문자열의 특정 위치 찾기 2/find-specific-location-in-spring-2.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
-
+#include<string.h>
 int main() {
-    char st;
-    char arr[5][20] = {"apple", "banana", "grape", "blueberry", "orange"};
+    char st,*p;
+    char str[5][20] = {"apple", "banana", "grape", "blueberry", "orange"};
     int i,cnt=0;
     scanf("%c",&st);
     for(i=0; i<5;i++)
     {
-        if(arr[i][2] ==st|| arr[i][3] ==st)
-        {
-            cnt++;
-            printf("%s\n",arr[i]);
-        }
+    	p=strchr(str[i]+2,st);
+    	if(p-str[i]==2||p-str[i]==3){
+            printf("%s\n",str[i]);
+    		cnt++;
+    }
     }
     printf("%d",cnt);
     return 0;
