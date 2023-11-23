9b9d7e76a4687af02a898ae20fabdb59644ba652
PupCat <105567668+PupCat@users.noreply.github.com>
2023-11-24 05:10:23 +0900

    Title: 문자열의 길이 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231124/문자열의 길이 출력하기/print-string's-range.c
+++ 231124/문자열의 길이 출력하기/print-string's-range.c
@@ -2,8 +2,8 @@
 #include <string.h>
 int main() {
     char arr[100], arr2[100];
-    scanf("%s", arr);
-    scanf("%s", arr2);
+    scanf("%s %s", arr, arr2);
+
     
     printf("%d", strlen(arr)+ strlen(arr2));
     return 0;
