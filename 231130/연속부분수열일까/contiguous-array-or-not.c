84fef54f4674632814d7797ed7835a12f4872414
PupCat <105567668+PupCat@users.noreply.github.com>
2023-11-30 16:45:10 +0900

    Title: 연속부분수열일까, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231130/연속부분수열일까/contiguous-array-or-not.c
+++ 231130/연속부분수열일까/contiguous-array-or-not.c
@@ -20,25 +20,25 @@ int main() {
 		// arr1의 i번 index부터의 수열이 arr2와 완전히 일치하는지 확인합니다.
 		// 즉, arr1[i] == arr2[0], arr1[i+1] == arr2[1]...이 성립하는지 확인합니다.
 		// success : arr1의 i번 index부터의 수열이 arr2와 완전히 일치할때만 true, 그 외 false
-		bool success = true;
+		int success = 1;
 		
 		for(int j = 0; j < n2; j++) {
 			// arr1의 index가 범위 밖으로 벗어날때
 			if(i + j >= n1) {
-				success = false;
+				success = 0;
 				break;
 			}
 			
 			// arr1과 arr2가 일치하지 않을때
 			if(arr1[i + j] != arr2[j]) {
-				success = false;
+				success = 0;
 				break;
 			}
 		}
 		
 		// 완전히 일치할 경우, arr2는 arr1의 연속부분수열이 맞습니다.
 		// 구현의 편의를 위해 return 0으로 프로그램을 강제 종료하였습니다.
-		if(success) {
+		if(success == 1) {
 			printf("Yes");
 			return 0;
 		}
